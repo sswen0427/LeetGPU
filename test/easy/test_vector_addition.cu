207759d6f310a204f5447c4f04e0805b5028f1a3
sswen0427 <799438891@qq.com>
2026-05-10 00:54:21 +0800

    remove extern "C"

--- test/easy/test_vector_addition.cu
+++ test/easy/test_vector_addition.cu
@@ -9,7 +9,7 @@ __global__ void vector_add(const float* A, const float* B, float* C, int N) {
 }
 
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
-extern "C" void solve(const float* A, const float* B, float* C, int N) {
+void solve(const float* A, const float* B, float* C, int N) {
   int threadsPerBlock = 256;
   int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
