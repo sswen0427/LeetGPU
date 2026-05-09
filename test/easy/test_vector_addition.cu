ec5eaae8901c8da87a08c4e4b6cbbf53c78f9c9b
sswen0427 <799438891@qq.com>
2026-05-09 23:02:47 +0800

    test_vector_addition

---
+++ test/easy/test_vector_addition.cu
@@ -0,0 +1,31 @@
+#include <cuda_runtime_api.h>
+#include <gtest/gtest.h>
+
+__global__ void vector_add(const float* A, const float* B, float* C, int N) {
+    unsigned int id = blockDim.x * blockIdx.x + threadIdx.x;
+    if (id < N) {
+        C[id] = A[id] + B[id];
+    }
+}
+
+// A, B, C are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* A, const float* B, float* C, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    cudaDeviceSynchronize();
+}
+
+
+// https://leetgpu.com/challenges/vector-addition
+TEST(LeetGPU, test1) {
+    float A[] = {1.0, 2.0, 3.0, 4.0};
+    float B[] = {5.0, 6.0, 7.0, 8.0};
+    float C[] = {0, 0, 0, 0};
+    solve(A, B, C, 4);
+    for (int i = 0; i < 4; i++) {
+        constexpr float expected[] = {6.0, 8.0, 10.0, 12.0};
+        EXPECT_EQ(expected[i], C[i]);
+    }
+}
